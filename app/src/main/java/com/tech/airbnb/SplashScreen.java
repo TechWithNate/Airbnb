89a9bf995ad37e26e5ad55d4277e40dbeedfc3a9
Tech With Nate <dzrekenathan2002@gmail.com>
2023-03-10 02:25:28 +0000

    Code Upgrade

---
+++ app/src/main/java/com/tech/airbnb/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.tech.airbnb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_TIMER = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIMER);
+
+    }
+}
